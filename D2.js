7a127252b4e69d368471e671028871a285262215
Marco Zagaria <marcozagaria@live.com>
2024-06-19 09:24:55 +0200

    fine

--- D2.js
+++ D2.js
@@ -72,18 +72,26 @@ if (blackFriday>50) {
   Alla fine mostra il risultato in console.
 */
 /* SCRIVI QUI LA TUA RISPOSTA */
-// const a=5
-// const b=15
-// const c=10
-// if (a>b) {
-//   if (c>a) {
-//     console.log(c,a,b)
-//   }else{
-//     if (c>b) {
-//       console.log(a,c,b)
-//     }else console.log(a,b,c)
-//   }
-// }
+const a=7
+const b=15
+const c=10
+if (a>b&&a>c) {
+  if (b>c) {
+    console.log(a,b,c)
+  }else {
+    console.log(a,c,b)
+  }} else if (b>a&&b>c) {
+    if (a>c) {
+      console.log(b,a,c)
+    }else{
+      console.log(b,c,a)
+    }
+  }else if (c>a) {
+    console.log(c,a,b)
+  }else{
+    console.log(c,b,a)
+  }
+
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
